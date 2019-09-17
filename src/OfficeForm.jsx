eb0989deeba7f185e29cc1f6e735d3518b6282ba
witcherovich <ua.bezprav@yandex.ua>
2019-09-17 11:31:18 +0300

    Created functionality to add new office

--- src/OfficeForm.jsx
+++ src/OfficeForm.jsx
@@ -4,9 +4,66 @@ import { connect } from 'react-redux';
 import { addOffice } from './actionCreators';
 
 class OfficeForm extends Component {
+	state = {
+		country: '',
+		stateOrProvince: '',
+		postalCode: '',
+		city: '',
+		streetAddress: '',
+		address2: '',
+		phone: '',
+		fax: '',
+		email: '',
+		officeType: false 
+	}
+
+	handleInputChange = ({ target: { name, value } }) => {
+		this.setState({ [name]: value });
+	}
+
+	handleSubmitForm = e => {
+		e.preventDefault();
+
+		this.props.addOffice({...this.state});
+	}
+
 	render() {
+		const { 
+			country, 
+			stateOrProvince, 
+			postalCode, 
+			city, 
+			streetAddress, 
+			address2, 
+			phone, 
+			fax, 
+			email, 
+			officeType 
+		} = this.state;
+
 		return (
-			<div className="OfficeForm"></div>
+			<div className="OfficeForm">
+				<form onSubmit={this.handleSubmitForm}>
+					<div>
+						<input className="OfficeForm__form_input" type="text" name="country" value={country} onChange={this.handleInputChange} />
+						<input className="OfficeForm__form_input" type="text" name="stateOrProvince" value={stateOrProvince} onChange={this.handleInputChange} />
+						<input className="OfficeForm__form_input" type="text" name="postalCode" value={postalCode} onChange={this.handleInputChange} />
+						<input className="OfficeForm__form_input" type="text" name="city" value={city} onChange={this.handleInputChange} />
+						<input className="OfficeForm__form_input" type="text" name="streetAddress" value={streetAddress} onChange={this.handleInputChange} />
+						<input className="OfficeForm__form_input" type="text" name="address2" value={address2} onChange={this.handleInputChange} />
+					</div>
+					<div>
+						<input className="OfficeForm__form_input" type="text" name="phone" value={phone} onChange={this.handleInputChange} />
+						<input className="OfficeForm__form_input" type="text" name="fax" value={fax} onChange={this.handleInputChange} />
+						<input className="OfficeForm__form_input" type="text" name="email" value={email} onChange={this.handleInputChange} />
+						<input className="OfficeForm__form_input" type="checkbox" name="officeType" value={officeType} onChange={this.handleInputChange} />
+					</div>
+					<div>
+						<button>Cancel</button>
+						<input type="submit" value="Save" />
+					</div>
+				</form>
+			</div>
 		);
 	}
 }
