f2edf56178e1c2b2a06fd7cab3e61446e22a4d3e
witcherovich <ua.bezprav@yandex.ua>
2019-09-17 19:35:44 +0300

    Created header

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import React, { Component } from 'react';
 import OfficeForm from './OfficeForm';
 import OfficeList from './OfficeList';
 
+import logoSpd from './images/logo_spd.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App__header">Header</div>
+        <div className="App__header App__header_text_styles">
+          <div className="App__header_left">
+            <img src={logoSpd} />
+            <span className="App__header_margin_left">Profile Editor</span>
+          </div>
+          <div className="App__header_rigt">
+            <span className="App__header_text">Contact</span>
+            <span className="App__header_text">FAQs</span>
+            <span className="App__header_text">Save and Exit</span>
+          </div>
+        </div>
         <div className="App__body">
           <div className="App__body_content">
             <div className="App__body_content_right">
