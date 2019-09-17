f36f58706d7272bab9463c72d867f144541ce336
witcherovich <ua.bezprav@yandex.ua>
2019-09-17 11:31:55 +0300

    Added OfficeForm component to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+
+import OfficeForm from './OfficeForm';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <OfficeForm />        
       </div>
     );
   }
