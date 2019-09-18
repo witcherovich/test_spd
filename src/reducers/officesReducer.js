7f1d2eee20e951d398faf7c1e942d41895fbab9b
witcherovich <ua.bezprav@yandex.ua>
2019-09-18 17:30:53 +0300

    Removed lastOfficeId

--- src/reducers/officesReducer.js
+++ src/reducers/officesReducer.js
@@ -1,13 +1,9 @@
 import { ADD_OFFICE, EDIT_OFFICE, REMOVE_OFFICE } from '../actionCreators';
 
-let lastOfficeId = 0;
-
 export default function officesReducer(state = [], { type, payload }) {
 	switch(type) {
 		case ADD_OFFICE:
 			const office = payload.office;
-			office.id = lastOfficeId;
-			lastOfficeId++;
 			return [...state, office];
 
 		case EDIT_OFFICE:
