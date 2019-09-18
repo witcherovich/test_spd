3275492e3a43ef37495f0bdc2cb652097ba34fd0
witcherovich <ua.bezprav@yandex.ua>
2019-09-18 17:29:08 +0300

    Added isString helper function

--- src/helperFunctions/index.js
+++ src/helperFunctions/index.js
@@ -1,3 +1,7 @@
 export function isNumber (value) {
 	return typeof value === 'number' && isFinite(value);
 }
+
+export function isString (value) {
+	return typeof value === 'string' || value instanceof String;
+}
