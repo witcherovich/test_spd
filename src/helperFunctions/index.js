1925f808582b753c7b82d91ede3b43fe5c2ba2e0
witcherovich <ua.bezprav@yandex.ua>
2019-09-19 02:02:40 +0300

    Removed useless function

--- src/helperFunctions/index.js
+++ src/helperFunctions/index.js
@@ -1,7 +1,3 @@
-export function isNumber (value) {
-	return typeof value === 'number' && isFinite(value);
-}
-
 export function isString (value) {
 	return typeof value === 'string' || value instanceof String;
 }
