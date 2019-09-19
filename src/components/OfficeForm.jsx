72a895b7943b0fa83e13570e31adf37918cb837e
witcherovich <ua.bezprav@yandex.ua>
2019-09-19 03:04:29 +0300

    Fixed bug with scroll app office

--- src/components/OfficeForm.jsx
+++ src/components/OfficeForm.jsx
@@ -72,11 +72,11 @@ class OfficeForm extends Component {
 							</div>
 							<div className="OfficeForm__form_row">
 								<div className="text__dark OfficeForm__form_label">*State/Province:</div>
-								<input className={`OfficeForm__form_field ${!stateOrProvince ? 'OfficeForm__form_field_invalid' : ''}`} type="text" name="stateOrProvince" value={stateOrProvince} onChange={this.handleInputChange} />
+								<input className={`OfficeForm__form_field ${!stateOrProvince ? 'OfficeForm__form_field_invalid' : ''}`} autoComplete="off" type="text" name="stateOrProvince" value={stateOrProvince} onChange={this.handleInputChange} />
 							</div>
 							<div className="OfficeForm__form_row">
 								<div className="text__dark OfficeForm__form_label">*Postal Code:</div>
-								<input className={`OfficeForm__form_field ${!postalCode ? 'OfficeForm__form_field_invalid' : ''}`} type="text" name="postalCode" value={postalCode} onChange={this.handleInputChange} />
+								<input className={`OfficeForm__form_field ${!postalCode ? 'OfficeForm__form_field_invalid' : ''}`} autoComplete="off" type="text" name="postalCode" value={postalCode} onChange={this.handleInputChange} />
 							</div>
 							<div className="OfficeForm__form_row">
 								<div className="text__dark OfficeForm__form_label">*City:</div>
@@ -88,11 +88,11 @@ class OfficeForm extends Component {
 							</div>
 							<div className="OfficeForm__form_row">
 								<div className="text__dark OfficeForm__form_label">*Street Andress:</div>
-								<input className={`OfficeForm__form_field ${!streetAddress ? 'OfficeForm__form_field_invalid' : ''}`} type="text" name="streetAddress" value={streetAddress} onChange={this.handleInputChange} />
+								<input className={`OfficeForm__form_field ${!streetAddress ? 'OfficeForm__form_field_invalid' : ''}`} autoComplete="off" type="text" name="streetAddress" value={streetAddress} onChange={this.handleInputChange} />
 							</div>
 							<div className="OfficeForm__form_row">
 								<div className="text__dark OfficeForm__form_label">Andress 2:</div>
-								<input className="OfficeForm__form_field" type="text" name="address2" value={address2} onChange={this.handleInputChange} />	
+								<input className="OfficeForm__form_field" autoComplete="off" type="text" name="address2" value={address2} onChange={this.handleInputChange} />	
 							</div>
 						</div>
 
@@ -100,20 +100,20 @@ class OfficeForm extends Component {
 							<div className="OfficeForm__form_column">
 								<div className="OfficeForm__form_row">
 									<div className="text__dark OfficeForm__form_label">Phone:</div>
-									<input className="OfficeForm__form_field" type="text" name="phone" value={phone} onChange={this.handleInputChange} />
+									<input className="OfficeForm__form_field" autoComplete="off" type="text" name="phone" value={phone} onChange={this.handleInputChange} />
 								</div>
 								<div className="OfficeForm__form_row">
 									<div className="text__dark OfficeForm__form_label">Fax:</div>
-									<input className="OfficeForm__form_field" type="text" name="fax" value={fax} onChange={this.handleInputChange} />
+									<input className="OfficeForm__form_field" autoComplete="off" type="text" name="fax" value={fax} onChange={this.handleInputChange} />
 								</div>
 								<div className="OfficeForm__form_row">
 									<div className="text__dark OfficeForm__form_label">Email:</div>
-									<input className="OfficeForm__form_field" type="text" name="email" value={email} onChange={this.handleInputChange} />
+									<input className="OfficeForm__form_field" autoComplete="off" type="text" name="email" value={email} onChange={this.handleInputChange} />
 								</div>
 								<div className="OfficeForm__form_row">
 									<div className="text__dark OfficeForm__form_label">Office Type:</div>
 									<div className="OfficeForm__form_checkbox_wrapper">
-										<input className="OfficeForm__form_checkbox" type="checkbox" name="officeType" checked={officeType} onChange={this.handleInputChange} />
+										<input className="OfficeForm__form_checkbox" autoComplete="off" type="checkbox" name="officeType" checked={officeType} onChange={this.handleInputChange} />
 										<div>Primary HQ</div>
 									</div>
 								</div>
