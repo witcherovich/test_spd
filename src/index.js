0c54a47db0bfe0838f2bc300ce3cae86772fd901
witcherovich <ua.bezprav@yandex.ua>
2019-09-20 15:48:51 +0300

    Added type checking to redux store

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
+// @flow
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import mainReducer from './reducers/';
+import type { Store } from './flow-types';
 
 import App from './components/App/App.jsx';
 
 import './index.css';
 
-const store = createStore(mainReducer, applyMiddleware(thunk));
+const store: Store = createStore(mainReducer, applyMiddleware(thunk));
+
+const root = document.getElementById('root');
+
+if (!root) {
+	throw new Error("Couldn't find element with id root");
+}
 
 ReactDOM.render(
 	<Provider store={store}>
 		<App />
 	</Provider>
-	, document.getElementById('root')
+	, root
 );
