39b8244e491cbc0265b1ebb2fe2c0675ec3b2692
witcherovich <ua.bezprav@yandex.ua>
2019-09-20 15:14:03 +0300

    Replaced custom dispatch with redux dispatch

--- src/flow-types/index.js
+++ src/flow-types/index.js
@@ -1,8 +1,12 @@
 // @flow
+import type { Store as ReduxStore, Dispatch as ReduxDispatch } from 'redux';
+
 import type { Office, OfficeFormActions } from '../flow-types/officeFormTypes';
 import type { OfficesState, OfficesActions } from '../flow-types/officesTypes';
 
 export type State = Office & OfficesState;
 export type Action = OfficesActions | OfficeFormActions;
+export type ThunkAction = (...args: Array<any>) => (dispatch: Dispatch) => any;
 
-export type Dispatch = (action: Action) => void;
+export type Store = ReduxStore<State, Action>;
+export type Dispatch = ReduxDispatch<Action | ThunkAction>;
