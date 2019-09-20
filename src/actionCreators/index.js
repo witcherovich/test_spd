ed0c8e418758b9e5162d0e92f4e7baef332721d8
witcherovich <ua.bezprav@yandex.ua>
2019-09-20 15:14:34 +0300

    Added type checking to action creators

--- src/actionCreators/index.js
+++ src/actionCreators/index.js
@@ -1,19 +1,24 @@
+// @flow
+import type { Action, Dispatch } from '../flow-types';
+import type { T_SET_OFFICES, T_ADD_OFFICE, T_EDIT_OFFICE, T_REMOVE_OFFICE } from '../flow-types/officesTypes';
+import type { Office, T_SET_INITIAL_FORM_STATE, T_SET_FORM_STATE, T_SET_FORM_FIELD } from '../flow-types/officeFormTypes';
+
 import firebase from '../firebaseConfig';
 const db = firebase.database().ref();
 
 // Office actions
-export const SET_OFFICES = 'SET_OFFICES';
-export const ADD_OFFICE = 'ADD_OFFICE';
-export const EDIT_OFFICE = 'EDIT_OFFICE';
-export const REMOVE_OFFICE = 'REMOVE_OFFICE';
+export const SET_OFFICES: T_SET_OFFICES = 'SET_OFFICES';
+export const ADD_OFFICE: T_ADD_OFFICE = 'ADD_OFFICE';
+export const EDIT_OFFICE: T_EDIT_OFFICE = 'EDIT_OFFICE';
+export const REMOVE_OFFICE: T_REMOVE_OFFICE = 'REMOVE_OFFICE';
 
 // Office form actions
-export const SET_INITIAL_FORM_STATE = 'SET_INITIAL_FORM_STATE';
-export const SET_FORM_STATE = 'SET_FORM_STATE';
-export const SET_FORM_FIELD = 'SET_FORM_FIELD';
+export const SET_INITIAL_FORM_STATE: T_SET_INITIAL_FORM_STATE = 'SET_INITIAL_FORM_STATE';
+export const SET_FORM_STATE: T_SET_FORM_STATE = 'SET_FORM_STATE';
+export const SET_FORM_FIELD: T_SET_FORM_FIELD = 'SET_FORM_FIELD';
 
 // Office action creators
-export const fetchOfficesDB = () => dispatch => {
+export const fetchOfficesDB = () => (dispatch: Dispatch): void => {
 	return db.child('/offices').once('value')
 		.then(data => {
 			const offices = Object.values(data.val()) || [];
@@ -22,7 +27,7 @@ export const fetchOfficesDB = () => dispatch => {
 		.catch(() => alert('Service Unavailable error message'));
 }
 
-export const saveOfficeDB = office => dispatch => {
+export const saveOfficeDB = (office: Office): any => (dispatch: Dispatch): void => {
 	const isEditMode = !!office.id;
 
 	const officeId = office.id || db.child('offices').push().key;
@@ -40,13 +45,13 @@ export const saveOfficeDB = office => dispatch => {
 		.catch(() => alert('Service Unavailable error message'));
 }
 
-export const removeOfficeDB = officeId => dispatch => {
+export const removeOfficeDB = (officeId: string): any => (dispatch: Dispatch): void => {
 	return db.child(`/offices/${officeId}`).remove()
 		.then(() => dispatch(removeOffice(officeId)))
 		.catch(() => alert('Service Unavailable error message'));
 }
 
-function setOffices(offices) {
+function setOffices(offices: Array<any>): Action {
 	return {
 		type: SET_OFFICES,
 		payload: {
@@ -55,7 +60,7 @@ function setOffices(offices) {
 	}
 }
 
-function addOffice(office) {
+function addOffice(office: Office): Action {
 	return {
 		type: ADD_OFFICE,
 		payload: {
@@ -64,7 +69,7 @@ function addOffice(office) {
 	}
 }
 
-function editOffice(office) {
+function editOffice(office: Office): Action {
 	return {
 		type: EDIT_OFFICE,
 		payload: {
@@ -73,7 +78,7 @@ function editOffice(office) {
 	}
 }
 
-function removeOffice(officeId) {
+function removeOffice(officeId: string): Action {
 	return {
 		type: REMOVE_OFFICE,
 		payload: {
@@ -83,13 +88,13 @@ function removeOffice(officeId) {
 }
 
 // Office form action creators
-export function setInitialFormState() {
+export function setInitialFormState(): Action {
 	return {
 		type: SET_INITIAL_FORM_STATE
 	}
 }
 
-export function setFormState(office) {
+export function setFormState(office: Office): Action {
 	return {
 		type: SET_FORM_STATE,
 		payload: {
@@ -98,7 +103,7 @@ export function setFormState(office) {
 	}
 }
 
-export function setFormField(field) {
+export function setFormField(field: Object): Action {
 	return {
 		type: SET_FORM_FIELD,
 		payload: {
