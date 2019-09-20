83156dc0a919226f29950161f1b94116907aaeab
witcherovich <ua.bezprav@yandex.ua>
2019-09-20 17:12:17 +0300

    Edited type checking in OfficeList

--- src/components/OfficeList/OfficeList.jsx
+++ src/components/OfficeList/OfficeList.jsx
@@ -3,13 +3,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import type { OfficesState } from '../../flow-types/officesTypes';
+import type { Office } from '../../flow-types/officeFormTypes';
 
 import OfficeItem from '../OfficeItem/OfficeItem';
 
 type OfficeListProps = {
   offices: OfficesState,
-  removeOffice: any,
-  editOffice: any
+  removeOffice: (officeId: string) => void,
+  editOffice: (office: Office) => void
 }
 
 class OfficeList extends Component<OfficeListProps> {
